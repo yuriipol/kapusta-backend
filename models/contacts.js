258cbea9ea60fb9e5de1effc022cab09dfa31990
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-10-25 14:30:43 +0300

    change task 2, refuctoring code

--- models/contacts.js
+++ models/contacts.js
@@ -42,13 +42,13 @@ const addContact = async ({ name, email, phone }) => {
   return newContact;
 };
 
-const updateContact = async (contactId, body) => {
+const updateContact = async (id, body) => {
   const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === contactId);
+  const index = contacts.findIndex((item) => item.id === id);
   if (index === -1) {
     return null;
   }
-  contacts[index] = { contactId, ...body };
+  contacts[index] = { id, ...body };
   await updateContacts(contacts);
   return contacts[index];
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,84 +2,28 @@ const express = require("express");
 
 const router = express.Router();
 
-const Joi = require("joi");
+const ctrl = require("../../controllers/contacts");
 
-const contacts = require("../../models/contacts");
+const { ctrlWrapper } = require("../../helpers");
+const { validateBody } = require("../../middlewares");
+const schemas = require("../../schemas/contats");
 
-const { RequestError } = require("../../helpers");
+router.get("/", ctrlWrapper(ctrl.listContacts));
 
-const bookAddSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
+router.get("/:contactId", ctrlWrapper(ctrl.getContactById));
 
-router.get("/", async (req, res, next) => {
-  try {
-    const result = await contacts.listContacts();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.post(
+  "/",
+  validateBody(schemas.contactAddSchema),
+  ctrlWrapper(ctrl.addContact)
+);
 
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.getContactById(contactId);
-    if (!result) {
-      throw RequestError(404, "Not found");
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete("/:contactId", ctrlWrapper(ctrl.removeContact));
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = bookAddSchema.validate(req.body);
-    if (error) {
-      throw RequestError(400, error.message);
-    }
-    const result = await contacts.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.removeContact(contactId);
-    if (!result) {
-      throw RequestError(404);
-    }
-    res.json({
-      message: "contact deleted",
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    const { error } = bookAddSchema.validate(req.body);
-    if (error) {
-      throw RequestError(400, error.message);
-    }
-    const { contactId } = req.params;
-    const result = await contacts.updateContact(contactId, req.body);
-    if (!result) {
-      throw RequestError(404);
-    }
-
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.put(
+  "/:contactId",
+  validateBody(schemas.contactAddSchema),
+  ctrlWrapper(ctrl.updateContact)
+);
 
 module.exports = router;
