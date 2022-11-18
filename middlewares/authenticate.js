f8d2db7ac7e29a451538989ef850e34a5e1e5dd7
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-11-18 09:48:36 +0200

    add register, login, loguot and current

---
+++ middlewares/authenticate.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+
+const { RequestError } = require("../helpers");
+
+const { User } = require("../models/user");
+
+const { SECRET_KEY } = process.env;
+
+const authenticate = async (req, res, next) => {
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+  if (bearer !== "Bearer") {
+    next(RequestError(401));
+  }
+  try {
+    const { id } = jwt.verify(token, SECRET_KEY);
+    const user = await User.findById(id);
+    console.log(user);
+    if (!user || !user.token) {
+      next(RequestError(401));
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("error message", error.message);
+    next(RequestError(401, error.message));
+  }
+};
+
+module.exports = authenticate;
--- models/contact.js
+++ models/contact.js
@@ -14,11 +14,15 @@ const contactSchema = new Schema(
     },
     phone: {
       type: String,
-      unique: true,
     },
     favorite: {
       type: Boolean,
       default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
     },
   },
   { versionKey: false, timestamps: true }
