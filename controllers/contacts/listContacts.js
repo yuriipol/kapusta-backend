fd16917aa941f0c24baca4b00cbe87ceaf55bcbd
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-11-20 11:53:06 +0200

    add filter favorite

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,11 +2,14 @@ const { Contact } = require("../../models/contact");
 
 const listContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit, favorite = false } = req.query;
+  const { page = 1, limit, favorite } = req.query;
 
   const skip = (page - 1) * limit;
+
+  const getFavoriteContact =
+    favorite !== undefined ? { owner, favorite } : { owner };
   const result = await Contact.find(
-    { owner, favorite },
+    getFavoriteContact,
     "-createdAt -updatedAt",
     {
       skip,
