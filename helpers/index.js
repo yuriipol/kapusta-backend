c466fec40869bb9e1235fcb0f10040c7b163ad9d
OlhaPetrenko <o.l.petr27@gmail.com>
2022-12-17 19:33:26 +0200

    add auth

--- helpers/index.js
+++ helpers/index.js
@@ -1,11 +1,9 @@
 const RequestError = require("./RequestError");
 const ctrlWrapper = require("./ctrlWrapper");
 const handleSaveErrors = require("./handleSaveErrors");
-const sendMail = require("./sendMail");
 
 module.exports = {
   RequestError,
   ctrlWrapper,
   handleSaveErrors,
-  sendMail,
 };
