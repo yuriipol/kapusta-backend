b0b19f404fafe3b807ab83025218d94bcec9d9d6
vovacelidze <louglescom@gmail.com>
2022-12-20 22:37:00 +0200

    finished basic functions of transaction

--- controllers/transaction/transactionService.js
+++ controllers/transaction/transactionService.js
@@ -3,7 +3,7 @@ const { TransactionModel } = require('../../models/transaction');
 const addTransactionExpenseService = async(data, user, owner) => {
   user.balance -= data.amount;
   await user.save();
-  const newTransaction = await TransactionModel.create({...data, owner})
+  const newTransaction = await TransactionModel.create({...data, type: 'expense', owner})
   return {status: 200, message: {
     "newBalance": data.amount,
     "transaction": newTransaction
@@ -13,7 +13,7 @@ const addTransactionExpenseService = async(data, user, owner) => {
 const addTransactionIncomeService = async(data, user, owner) => {
   user.balance += data.amount;
   await user.save();
-  const newTransaction = await TransactionModel.create({...data, category: "income", owner})
+  const newTransaction = await TransactionModel.create({...data, type: 'income', owner})
   return {status: 200, message: {
     "newBalance": data.amount,
     "transaction": newTransaction
@@ -21,26 +21,21 @@ const addTransactionIncomeService = async(data, user, owner) => {
 }
 
 const getTransactionIncomeService = async () => {
-  const data = await TransactionModel.find({})
+  const data = await TransactionModel.find({type: 'income'})
+  return {status: 200, message: data}
 }
 
 const getTransactionExpenseService = async () => {
-  
+  const data = await TransactionModel.find({type: 'expense'})
+  return {status: 200, message: data}
 }
 
 const deleteTransactionService = async(_id, owner, user) => {
   const transaction = await TransactionModel.findByIdAndRemove({_id, owner}, {returnDocument: 'before'});
-  if(transaction.category === 'income'){
-    user.balance -= transaction.amount;
-    await user.save();
-  }
-  if(transaction.category !== 'income'){
-    user.balance += transaction.amount;
-    await user.save();
-  }
+  if(transaction.type === 'income') user.balance -= transaction.amount, await user.save();
+  if(transaction.type === 'expense') user.balance += transaction.amount, await user.save();
   return {status: 200, message: {"newBalance": user.balance}}
 }
-
 
 module.exports = {
   addTransactionExpenseService,
